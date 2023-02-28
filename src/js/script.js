ff1739c0e74d88178e91c250b90a82aa4f0d278e
Alexey <adduction@mail.ru>
2023-02-28 18:55:27 +0300

    add Mask for phone JS

--- src/js/script.js
+++ src/js/script.js
@@ -229,7 +229,6 @@ document.addEventListener("DOMContentLoaded", () => {
                             modalDescr.textContent = `${subtitle[i].textContent}`;
                         });
                     }
-                    openModal();
                 });
             });
         }
@@ -238,4 +237,63 @@ document.addEventListener("DOMContentLoaded", () => {
     bindModal('[data-modal="consult"]', "#consult", "#consult .modal__close");
     bindModal('[data-modal="thanks"]', "#thanks", "#thanks .modal__close");
     bindModal(".button_catalogue", "#order", "#order .modal__close");
+
+    const phoneInputs = document.querySelectorAll('input[name="phone"]');
+
+    function getInputNumbersValue(input) {
+        return input.value.replace(/\D/g, "");
+    }
+
+    function onPhoneInput(e) {
+        let input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            formattedInputValue = "",
+            selectionStart = input.selectionStart;
+
+        if (!inputNumbersValue) {
+            return (input.value = "");
+        }
+
+        if (input.value.length != selectionStart) {
+            if (e.data && /\D/g.test(e.data)) {
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+            if (inputNumbersValue[0] == "9") {
+                inputNumbersValue = "7" + inputNumbersValue;
+            }
+            let firstSymbols = inputNumbersValue[0] == "8" ? "8" : "+7";
+            formattedInputValue = firstSymbols + " ";
+            if (inputNumbersValue.length > 1) {
+                formattedInputValue += "(" + inputNumbersValue.substring(1, 4);
+            }
+            if (inputNumbersValue.length >= 5) {
+                formattedInputValue += ") " + inputNumbersValue.substring(4, 7);
+            }
+            if (inputNumbersValue.length >= 8) {
+                formattedInputValue += "-" + inputNumbersValue.substring(7, 9);
+            }
+            if (inputNumbersValue.length >= 10) {
+                formattedInputValue += "-" + inputNumbersValue.substring(9, 11);
+            }
+        } else {
+            formattedInputValue = "+" + inputNumbersValue.substring(0, 16);
+        }
+        input.value = formattedInputValue;
+    }
+
+    function onPhoneKeyDown(e) {
+        let input = e.target;
+        if (e.keyCode == 8 && getInputNumbersValue(input).length == 1) {
+            input.value = "";
+        }
+    }
+
+    phoneInputs.forEach((item) => {
+        item.addEventListener("input", onPhoneInput);
+        item.addEventListener("keydown", onPhoneKeyDown);
+    });
 });
